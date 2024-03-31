a7974dea67b4dc0b2ec11b443f450eb45c678484
Kaval Patel <kaval@devrepublic.nl>
2024-03-31 17:22:14 +0530

    31-03-2024 Changes Done

--- SMSystem Api/Model/BaseEntityModel.cs
+++ SMSystem Api/Model/BaseEntityModel.cs
@@ -2,10 +2,10 @@
 {
     public class BaseEntityModel
     {
-        public string CreatedBy { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public string ModifiedBy { get; set; }
-        public DateTime ModifiedDate { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
         public bool IsActive { get; set; }
         public bool IsDelete { get; set; }
     }
